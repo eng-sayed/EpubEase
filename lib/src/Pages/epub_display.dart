4b4a98d03db7d88fd9a6ac2df4a18b31418f6078
Maksym Loia <knemo7362@gmail.com>
2023-08-21 15:16:32 +0300

    test

--- lib/src/Pages/epub_display.dart
+++ lib/src/Pages/epub_display.dart
@@ -431,7 +431,7 @@ class Home extends State<ShowEpub> {
   Widget build(BuildContext context) {
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       if (wasInit && controller.hasListeners) {
-        await Future.delayed(const Duration(milliseconds: 300));
+        await Future.delayed(const Duration(milliseconds: 1100));
         if (wasInit) {
           controller.jumpTo(
             controller.position.maxScrollExtent *
@@ -468,12 +468,6 @@ class Home extends State<ShowEpub> {
                                   child: HtmlWidget(
                                     htmlcontent,
                                     buildAsync: true,
-                                    onLoadingBuilder: (context, element,
-                                            loadingProgress) =>
-                                        SingleChildScrollView(
-                                            child: CircularProgressIndicator(
-                                      value: loadingProgress,
-                                    )),
                                     renderMode:
                                         ListViewMode(controller: controller),
                                     factoryBuilder: () => _CustomWidgetFactory(
