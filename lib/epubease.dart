d3675a069cc66ce6874c004511567238e08dcf07
Maksym Loia <knemo7362@gmail.com>
2023-09-12 09:34:15 +0300

    test

--- lib/epubease.dart
+++ lib/epubease.dart
@@ -1,6 +1,7 @@
 library epubease;
 
 import 'dart:async';
+import 'dart:io';
 
 import 'package:epubease/src/Model/chapter_model.dart';
 import 'package:epubease/src/Model/last_place_model.dart';
@@ -11,8 +12,6 @@ import 'package:epubx/epubx.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import 'package:flutter/services.dart' show rootBundle;
-
 import 'epubease.dart';
 
 export 'src/Pages/epub_display.dart';
@@ -22,47 +21,6 @@ class Epubease {
   static final GlobalKey<NavigatorState> navigatorKey =
       GlobalKey<NavigatorState>();
 
-  static Future<void> openAsset(
-    String assetpath,
-    BuildContext context, {
-    required ReaderResult result,
-    required Function(ReaderResult result) onClose,
-    required Function(ReaderResult result) onSave,
-    required Future<dynamic> Function(Widget reader) pushReader,
-  }) async {
-    var bytes = await rootBundle.load(assetpath);
-
-    EpubBook epubBook = await EpubReader.readBook(bytes.buffer.asUint8List());
-
-    String htmlcontent = '';
-    Map<String, EpubTextContentFile> htmlFiles = epubBook.Content!.Html!;
-
-    for (var htmlFile in htmlFiles.values) {
-      htmlcontent = htmlcontent + htmlFile.Content!;
-    }
-
-    final chaptersPercentages =
-        dealWithChapters(result.chapters, epubBook.Chapters ?? []);
-    final realLastPlace =
-        dealWithLastPlace(chaptersPercentages, result.lastPlace);
-
-    final repository = Repository(onSave: onSave, lastReadResult: result);
-
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      final result = await pushReader(
-        ShowEpub(
-          html1: htmlcontent,
-          epubBook: epubBook,
-          lastPlace: realLastPlace,
-          repository: repository,
-          realChapters: chaptersPercentages,
-        ),
-      );
-
-      onClose(result);
-    });
-  }
-
   static Future<void> open(
     String bookurl,
     BuildContext context, {
@@ -70,40 +28,48 @@ class Epubease {
     required Function(ReaderResult result) onClose,
     required Function(ReaderResult result) onSave,
     required Future<dynamic> Function(Widget reader) pushReader,
+    required void Function(String message) onError,
   }) async {
-    final response = await http.get(Uri.parse(bookurl));
-    if (response.statusCode == 200) {
-      final epubData = response.bodyBytes;
+    try {
+      final response = await http.get(Uri.parse(bookurl));
+      if (response.statusCode == 200) {
+        final epubData = response.bodyBytes;
 
-      // Load the EPUB from memory
-      EpubBook epubBook = await EpubReader.readBook(epubData);
+        // Load the EPUB from memory
+        EpubBook epubBook = await EpubReader.readBook(epubData);
 
-      String htmlcontent = '';
-      Map<String, EpubTextContentFile> htmlFiles = epubBook.Content!.Html!;
+        String htmlcontent = '';
+        Map<String, EpubTextContentFile> htmlFiles = epubBook.Content!.Html!;
 
-      for (var htmlFile in htmlFiles.values) {
-        htmlcontent = htmlcontent + htmlFile.Content!;
+        for (var htmlFile in htmlFiles.values) {
+          htmlcontent = htmlcontent + htmlFile.Content!;
+        }
+
+        final chaptersPercentages =
+            dealWithChapters(result.chapters, epubBook.Chapters ?? []);
+        final realLastPlace =
+            dealWithLastPlace(chaptersPercentages, result.lastPlace);
+
+        final repository = Repository(onSave: onSave, lastReadResult: result);
+
+        WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+          final result = await pushReader(
+            ShowEpub(
+              html1: htmlcontent,
+              epubBook: epubBook,
+              lastPlace: realLastPlace,
+              repository: repository,
+              realChapters: chaptersPercentages,
+            ),
+          );
+          onClose(result);
+        });
       }
-
-      final chaptersPercentages =
-          dealWithChapters(result.chapters, epubBook.Chapters ?? []);
-      final realLastPlace =
-          dealWithLastPlace(chaptersPercentages, result.lastPlace);
-
-      final repository = Repository(onSave: onSave, lastReadResult: result);
-
-      WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-        final result = await pushReader(
-          ShowEpub(
-            html1: htmlcontent,
-            epubBook: epubBook,
-            lastPlace: realLastPlace,
-            repository: repository,
-            realChapters: chaptersPercentages,
-          ),
-        );
-        onClose(result);
-      });
+      throw HttpException(response.body);
+    } on HttpException catch (e) {
+      onError(e.message);
+    } catch (e) {
+      onError(e.toString());
     }
   }
 }
