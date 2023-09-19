cb242e317098e8332122f94e4dcf33cb400f031b
Maksym Loia <knemo7362@gmail.com>
2023-09-19 12:08:51 +0300

    test

--- lib/src/Component/arrow_back_button.dart
+++ lib/src/Component/arrow_back_button.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+const arrowSvg =
+    """<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9 5L2.5 11.5L9 18" stroke="#FEF9F2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M22 11.5L3.58334 11.5" stroke="#FEF9F2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>""";
+
 class ArrowBackButton extends StatelessWidget {
   final VoidCallback onPressed;
   const ArrowBackButton({required this.onPressed, super.key});
@@ -10,8 +16,8 @@ class ArrowBackButton extends StatelessWidget {
     return IconButton(
       onPressed: onPressed,
       iconSize: 24,
-      icon: SvgPicture.asset(
-        "assets/icons/arrow_left.svg",
+      icon: SvgPicture.string(
+        arrowSvg,
         height: 24,
         width: 24,
       ),
